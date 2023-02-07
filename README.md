fc8e7a415346a4f211a7f54fa5ed76c973539007
Meinte Boersma <meinte.boersma@gmail.com>
2023-02-07 14:44:57 +0100

    implement JSON with Freon

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Freon DSLs
 
 This repository contains a couple of DSLs implemented with [Freon](https://www.freon4dsl.dev/) - also see [its GitHub org](https://github.com/freon4dsl/).
+
 The purpose of these DSL implementations is to serve as examples, and are provided as-is (without guarantee and/or support) under the Apache 2.0 License.
+
+The implementations are derived from the [Freon template](https://github.com/freon4dsl/Freon-template).
+
+Freon was recently renamed from “ProjectIt”, and some implementations might not be switched over completely.
+
+
+## DSLs
+
+* [JSON](./freon-json) - (template used was the ProjectIt one)
 
---
+++ freon-json/src/webapp/config/WebappConfiguration.ts
@@ -0,0 +1,17 @@
+import type { PiEnvironment } from "@projectit/core";
+import { LanguageInitializer } from "../language/LanguageInitializer";
+import type { IServerCommunication } from "../server/IServerCommunication";
+import { ServerCommunication } from "../server/ServerCommunication";
+
+/**
+ * The one and only reference to the actual language for which this editor runs
+ */
+import { JsonEnvironment } from "../../picode/config/gen/JsonEnvironment";
+export const editorEnvironment: PiEnvironment = JsonEnvironment.getInstance();
+LanguageInitializer.initialize();
+
+/**
+ * The one and only reference to the server on which the models are stored
+ */
+export const serverCommunication: IServerCommunication = ServerCommunication.getInstance();
+// export const serverCommunication: IServerCommunication = MpsServerCommunication.getInstance();
